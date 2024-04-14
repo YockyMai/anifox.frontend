e6da8c7b2dd95dd20421ca78edc3dfc3f131a66f
DanilFrontend <danilstorage@gmail.com>
2024-04-14 14:23:59 +0500

    ✨ feat: [footer] `footer`.

--- src/modules/footer/footer.tsx
+++ src/modules/footer/footer.tsx
@@ -1,3 +1,12 @@
+import { Link } from 'react-router-dom'
+
+import './footer.scss'
+
 export const Footer = () => {
-  return <footer className={'site_footer'}>footer</footer>
+  return (
+    <footer className={'site_footer'}>
+      <Link to={'#'}>{'Для правообладателей'}</Link>
+      <p>{`© anifox.club 2023 - ${new Date().getFullYear()}`}</p>
+    </footer>
+  )
 }
