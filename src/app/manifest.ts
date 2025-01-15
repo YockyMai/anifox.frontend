421314ff40864654348e53d3f29854b1181aa38a
YockyMai <g.valera15102003@gmail.com>
2025-01-15 17:01:47 +0500

    ✨ feat: added manifest file

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'ANIFOX',
+    short_name: 'AniFox',
+    description: 'Смотри, отслеживай и наслаждайся любимыми аниме онлайн!',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#1e293b',
+    theme_color: '#FFFFFF',
+    icons: [
+      {
+        src: '/logo/256x256.webp',
+        sizes: '256x256',
+        type: 'image/webp'
+      },
+      {
+        src: '/logo/512x512.webp',
+        sizes: '512x512',
+        type: 'image/webp'
+      }
+    ]
+  }
+}
