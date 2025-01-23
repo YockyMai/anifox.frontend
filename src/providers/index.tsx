1f5bea7f44055ff3a78bc6ab006e4387db2cd8a1
YockyMai <g.valera15102003@gmail.com>
2025-01-23 10:54:00 +0500

    ♻️ refactor: removed `jotail` provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,21 +1,18 @@
 import { ReactNode } from 'react'
 
 import { WithAuthProvider } from './with-auth-provider'
-import { WithJotaiProvider } from './with-jotai-provider'
 import { WithReactQueryProvider } from './with-react-query-provider'
 import { WithSiteTheme } from './with-site-theme'
 import { WithWindowProvider } from './with-window-provider'
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <WithJotaiProvider>
-      <WithReactQueryProvider>
-        <WithAuthProvider>
-          <WithWindowProvider>
-            <WithSiteTheme>{children}</WithSiteTheme>
-          </WithWindowProvider>
-        </WithAuthProvider>
-      </WithReactQueryProvider>
-    </WithJotaiProvider>
+    <WithReactQueryProvider>
+      <WithAuthProvider>
+        <WithWindowProvider>
+          <WithSiteTheme>{children}</WithSiteTheme>
+        </WithWindowProvider>
+      </WithAuthProvider>
+    </WithReactQueryProvider>
   )
 }
--- src/providers/with-jotai-provider.tsx
+++
@@ -1,6 +0,0 @@
-import { Provider } from 'jotai'
-import { ReactNode } from 'react'
-
-export const WithJotaiProvider = ({ children }: { children: ReactNode }) => {
-  return <Provider>{children}</Provider>
-}
