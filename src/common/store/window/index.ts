a2bea8abd624f92e958b940f709bd6ed9bdd08b1
YockyMai <g.valera15102003@gmail.com>
2025-02-17 03:16:54 +0500

    ♻️ refactor: using `useWindowWith` hook instead of `jotail` state

--- src/common/store/window/index.ts
+++
@@ -1,1 +0,0 @@
-export * as $windowAtoms from './window'
--- src/common/store/window/window.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'jotai'
-
-export const windowWidth = atom(
-  typeof window !== 'undefined' ? window.innerWidth : 1920
-)
-export const windowHeight = atom(
-  typeof window !== 'undefined' ? window.innerHeight : 1080
-)
