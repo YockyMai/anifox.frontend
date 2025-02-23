2f71ba0197ac24ee86e9f5e9309fd3f09982105f
YockyMai <g.valera15102003@gmail.com>
2025-02-24 02:01:37 +0500

    🐛 fix: exit from account after expiration of `accessToken`

--- src/app/providers/with-auth-provider/with-auth-provider.tsx
+++ src/app/providers/with-auth-provider/with-auth-provider.tsx
@@ -4,19 +4,47 @@ import { ReactNode, useEffect } from 'react'
 
 import { COOKIES } from '@/common/const'
 import { $viewer, User } from '@/entities/viewer'
+import { http } from '@/services/http'
 
 export const WithAuthProvider = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
-    const accessToken = cookie.parse(document.cookie)[COOKIES.ACCESS_TOKEN_KEY]
+    const initUser = async () => {
+      const accessToken = cookie.parse(document.cookie)[
+        COOKIES.ACCESS_TOKEN_KEY
+      ]
 
-    if (accessToken) {
-      // TODO: как будет запрос /me, запрашивать пользователя по токену
-      const user: User = jwtDecode(accessToken)
+      if (accessToken) {
+        // TODO: как будет запрос /me, запрашивать пользователя по токену
+        const user: User = jwtDecode(accessToken)
 
-      if (user) {
-        $viewer.actions.setViewer(user)
+        if (user) {
+          $viewer.actions.setViewer(user)
+        }
+      } else {
+        const refreshToken = cookie.parse(document.cookie)[
+          COOKIES.REFRESH_TOKEN_KEY
+        ]
+
+        if (refreshToken) {
+          await http.get('/auth/refreshToken', {
+            params: { refreshToken },
+            headers: {}
+          })
+
+          const accessToken = cookie.parse(document.cookie)[
+            COOKIES.ACCESS_TOKEN_KEY
+          ]
+
+          const user: User = jwtDecode(accessToken)
+
+          if (user) {
+            $viewer.actions.setViewer(user)
+          }
+        }
       }
     }
+
+    initUser()
   }, [])
 
   return children
